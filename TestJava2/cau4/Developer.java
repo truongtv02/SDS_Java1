7ceb4b60100c785d02e159ae023ff9bca790db97
truongtv02 <71001274+truongtv02@users.noreply.github.com>
2023-03-09 10:46:17 +0700

    commit

---
+++ TestJava2/cau4/Developer.java
@@ -0,0 +1,12 @@
+package TestJava2.cau4;
+
+public class Developer extends Person {
+    public Developer(String name, int age, String address) {
+        super(name, age, address);
+    }
+
+    @Override
+    public void speak() {
+
+    }
+}
